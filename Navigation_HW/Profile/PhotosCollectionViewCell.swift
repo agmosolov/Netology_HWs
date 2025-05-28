be95118fe596da840151f86cfe10deac20562cdc
Aleksandr Mosolov <agmosolov@gmail.com>
2025-05-28 23:32:24 +0300

    Home Work #6 completed

---
+++ Navigation_HW/Profile/PhotosCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Navigation_HW
+//
+//  Created by Александр Мосолов on 28.05.2025.
+//
+
+import Foundation
---
+++ Navigation_HW/Profile/PhotosViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PhotosViewController.swift
+//  Navigation_HW
+//
+//  Created by Александр Мосолов on 28.05.2025.
+//
+
+import Foundation
