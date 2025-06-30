ba9c6c0f71915c4acb8fd6130e93bad17346b667
Aleksandr Mosolov <agmosolov@gmail.com>
2025-06-30 20:20:45 +0300

    Add StorageSevice Target

---
+++ StorageService/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  Navigation_HW
+//
+//  Created by Александр Мосолов on 06.05.2025.
+//
+
+//struct Post {
+//    let author: String
+//    let description: String
+//    let image: String
+//    let likes: Int
+//    let views: Int
+//}
+
