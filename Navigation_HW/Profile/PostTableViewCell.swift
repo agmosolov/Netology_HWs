931e94e3f99b3071b4da23696ac103a4a2f113ac
Aleksandr Mosolov <agmosolov@gmail.com>
2025-07-03 22:35:26 +0300

    HW Ind-Dev #2 Completed

--- Navigation_HW/Profile/PostTableViewCell.swift
+++ Navigation_HW/Profile/PostTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import StorageService
-
+import iOSIntPackage
 
 final class PostTableViewCell: UITableViewCell {
     
@@ -16,6 +16,8 @@ final class PostTableViewCell: UITableViewCell {
     private let postImageView = UIImageView()
     private let likesLabel = UILabel()
     private let viewsLabel = UILabel()
+    
+    private let imageProcessor = ImageProcessor() // Создаем экземпляр ImageProcessor
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -31,7 +33,6 @@ final class PostTableViewCell: UITableViewCell {
         authorLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         authorLabel.textColor = .black
         authorLabel.numberOfLines = 2
-        
         
         descriptionLabel.numberOfLines = 0
         descriptionLabel.textColor = .systemGray
@@ -73,12 +74,19 @@ final class PostTableViewCell: UITableViewCell {
             viewsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
         ])
     }
-      
-      func configure(with post: Post) {
-          authorLabel.text = post.author
-          descriptionLabel.text = post.description
-          postImageView.image = UIImage(named: post.image)
-          likesLabel.text = "Likes: \(post.likes)"
-          viewsLabel.text = "Views: \(post.views)"
-      }
-  }
+    
+    func configure(with post: Post) {
+        authorLabel.text = post.author
+        descriptionLabel.text = post.description
+        likesLabel.text = "Likes: \(post.likes)"
+        viewsLabel.text = "Views: \(post.views)"
+        
+        if let image = UIImage(named: post.image) {
+            imageProcessor.processImage(sourceImage: image, filter: .sepia(intensity: 0.8)) { [weak self] processedImage in
+                DispatchQueue.main.async {
+                    self?.postImageView.image = processedImage
+                }
+            }
+        }
+    }
+}
