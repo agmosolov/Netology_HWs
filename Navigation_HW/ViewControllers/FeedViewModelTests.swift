74cce52d9f87427119fdd4f7117a2764ba32c746
Aleksandr Mosolov <agmosolov@gmail.com>
2025-11-08 20:27:04 +0300

    HW Completd Add

--- Navigation_HW/ViewControllers/FeedViewModelTests.swift
+++ Navigation_HW/ViewControllers/FeedViewModelTests.swift
@@ -5,4 +5,65 @@
 //  Created by Александр Мосолов on 08.11.2025.
 //
 
-import Foundation
+import XCTest
+@testable import Navigation_HW
+
+final class FeedViewModelTests: XCTestCase {
+    
+    var viewModel: FeedViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = FeedViewModel()
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
+    }
+    
+    func testCheckGuessWithEmptyWord() {
+        var statusMessage: String?
+        var statusColor: UIColor?
+        
+        viewModel.statusText = { message, color in
+            statusMessage = message
+            statusColor = color
+        }
+        
+        viewModel.checkGuess(word: "")
+        
+        XCTAssertEqual(statusMessage, "Поле не должно быть пустым")
+        XCTAssertEqual(statusColor, .red)
+    }
+    
+    func testCheckGuessWithCorrectWord() {
+        var statusMessage: String?
+        var statusColor: UIColor?
+        
+        viewModel.statusText = { message, color in
+            statusMessage = message
+            statusColor = color
+        }
+        
+        viewModel.checkGuess(word: "swift")
+        
+        XCTAssertEqual(statusMessage, "Верно!")
+        XCTAssertEqual(statusColor, .systemGreen)
+    }
+    
+    func testCheckGuessWithIncorrectWord() {
+        var statusMessage: String?
+        var statusColor: UIColor?
+        
+        viewModel.statusText = { message, color in
+            statusMessage = message
+            statusColor = color
+        }
+        
+        viewModel.checkGuess(word: "wrong")
+        
+        XCTAssertEqual(statusMessage, "Не верно!")
+        XCTAssertEqual(statusColor, .red)
+    }
+}
